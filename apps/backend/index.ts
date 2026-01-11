602ee25431006a93502c58dab7f6af9abfdbb324
GIT-Saikat <saikatmondal2105@gmail.com>
2026-01-11 19:01:28 +0530

    initial backend

---
+++ apps/backend/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.post("/ai/training",(req,res)=>{
+
+})
+
+app.post("/ai/generate0",(req,res)=>{
+
+})
+
+app.post("/package/generate",(req,res)=>{
+
+})
+
+app.get("/package/bulk",(req,res)=>{
+
+})
+
+app.get("/image",(req,res)=>{
+    
+})
+
+app.listen(PORT,()=>{
+    console.log("server is running on 3000");
+})
